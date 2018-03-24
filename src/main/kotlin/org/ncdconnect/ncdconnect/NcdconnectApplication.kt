967aa23c3f57796ed7ab8e3f566a2d317b0499cf
Angus Goldsmith <durins_beard@hotmail.com>
2018-03-24 20:06:43 +0000

    Create project using Spring Initialzr.

---
+++ src/main/kotlin/org/ncdconnect/ncdconnect/NcdconnectApplication.kt
@@ -0,0 +1,11 @@
+package org.ncdconnect.ncdconnect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NcdconnectApplication
+
+fun main(args: Array<String>) {
+    runApplication<NcdconnectApplication>(*args)
+}
---
+++ src/test/kotlin/org/ncdconnect/ncdconnect/NcdconnectApplicationTests.kt
@@ -0,0 +1,16 @@
+package org.ncdconnect.ncdconnect
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class NcdconnectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
